acf5725f0795e93733bb6c7cafd1c194266a7bb9
Daniel S <iam@w-b.dev>
2020-11-23 21:14:57 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span>Header</span>
+          <button>Login</button>
       </header>
+      <main className="App-main">
+        <span>Main</span>
+      </main>
+      <footer className="App-footer">
+        <span>Footer</span>
+      </footer>
     </div>
   );
 }
